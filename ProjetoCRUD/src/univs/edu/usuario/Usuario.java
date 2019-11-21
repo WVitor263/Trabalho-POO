a76113502ac8da59ef1d3f6b146f5d429d1b7c34
Weliton Vitor <wellingtonvictoralves@gmail.com>
2019-11-21 15:44:36 -0300

    Inicio

---
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -0,0 +1,52 @@
+
+package univs.edu.usuario;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue
+ private int idUsuario;
+    @Column(length = 100, nullable = false)
+  private String nomeUsuario;
+      @Column(length = 100, nullable = false)
+  private String login;
+          @Column(length = 100, nullable = false)
+  private String senha;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
@@ -0,0 +1,59 @@
+
+package univs.edu.usuario;
+
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import univs.edu.util.HibernateUtil;
+
+
+public class UsuarioDAO {
+    private Session sessao;
+    private Transaction transacao;
+    
+    public void salvar(Usuario usuario){
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        if(usuario.getIdUsuario() ==0){
+        sessao.save(usuario);
+       JOptionPane.showMessageDialog(null, "Usuario Cadastrado");
+    }else{
+        sessao.update(usuario);
+        JOptionPane.showMessageDialog(null, "Usuario Editado");
+        }
+        transacao.commit();
+        sessao.close();
+    }
+     public void excluir(Usuario usuario){
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        sessao.delete(usuario);
+        transacao.commit();
+        sessao.close();
+    }
+     public Usuario pesquisar(int id){
+         sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        Usuario usuario = (Usuario) sessao.createCriteria(Usuario.class).add(Restrictions.eq("idUsuario", id)).uniqueResult();
+        sessao.close();
+        return usuario;
+        
+     }
+      public List<Usuario> pesquisar(){
+         sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        List<Usuario> usuarios = sessao.createCriteria(Usuario.class).list();
+        return usuarios;
+        
+     }
+      public void editarUsuario(Usuario usuario){
+          sessao = HibernateUtil.getSessionFactory().openSession();
+        transacao = sessao.beginTransaction();
+        sessao.update(usuario);
+        transacao.commit();
+        sessao.close();
+      }
+     
+}
